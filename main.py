ac85e99bfe8515d406f6dcfa5ddf688bb2cbc272
ryosyntaxx <riodwi12174@gmail.com>
2024-02-22 15:11:27 +0700

    feat: linkedin completed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src.service.instagram import Instagram
 from src.service.tiktok import Tiktok
 from src.service.facebook import Facebook
+from src.service.linkedin import Linkedin
 
 if __name__ == '__main__':
-    ig = Facebook()
-    print(ig.main('hartonoahmadjaizy'))
+    ig = Linkedin()
+    print(ig.main('https://www.linkedin.com/in/herlambang-kurniawan-5b2692288/'))
     ...
---
+++ src/service/linkedin.py
@@ -0,0 +1,37 @@
+import os
+import requests
+
+from icecream import ic
+from requests import Response
+from typing import Dict
+from dotenv import load_dotenv
+
+
+class Linkedin:
+    def __init__(self) -> None:
+        load_dotenv()
+
+        self.api: str = os.getenv('API')+'/linkedin/search/account?limit=1&name='
+        ...
+
+    def build_response(self, data: dict) -> Dict[str, any]:
+        data: dict = data["data"][0]
+
+        return {
+            "name": data["name"],
+            "uid": data["urn_id"]
+        }
+        ...
+
+    def build_name(self, name: str) -> str:
+        name: str = name if 'https://www.linkedin.com/in/' not in name else name.replace('https://www.linkedin.com/in/', '')
+        
+        return '-'.join(name.split('-')[:-1])
+        ...
+
+    def main(self, name: str) -> Dict[str, any]:
+        response: Response = requests.get(self.api+name)
+        return self.build_response(response.json())
+        ...
+
+    
